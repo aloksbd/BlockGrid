cbe36d57780a99e8a42de9c01076513ca0e10950
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-03-24 16:49:39 +0545

    Piece shadow position calculated

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ document.onmouseup = function (mouse) {
     if (currentPiece.isPicked){
         var mouseX = mouse.clientX - canvas.getBoundingClientRect().left;
         var mouseY = mouse.clientY - canvas.getBoundingClientRect().top;
-        let matchedCount = board.placePiece(currentPiece.shape,mouseX-currentPiece.width()/2+currentPiece.boxSize/2,mouseY-currentPiece.height()/2+currentPiece.boxSize/2);
+        let matchedCount = board.placePiece(currentPiece.shape,mouseX-boardX-currentPiece.width()/2+currentPiece.boxSize/2,mouseY-boardY-currentPiece.height()/2+currentPiece.boxSize/2);
         scoreBoard.score += matchedCount;
         if (board.wall == 0){
             scoreBoard.score += scoreBoard.level * 5;
@@ -118,7 +118,7 @@ document.onmousemove = function (mouse) {
         var mouseY = mouse.clientY - canvas.getBoundingClientRect().top;
         currentPiece.picked(mouseX,mouseY);
         draw();
-        board.checkPlacebale(currentPiece.shape,mouseX-currentPiece.width()/2+currentPiece.boxSize/2,mouseY-currentPiece.height()/2+currentPiece.boxSize/2);
+        board.checkPlacebale(currentPiece.shape,mouseX-boardX-currentPiece.width()/2+currentPiece.boxSize/2,mouseY-boardY-currentPiece.height()/2+currentPiece.boxSize/2);
     }
     if (powerUp != null){
         if (powerUp.isPicked){
