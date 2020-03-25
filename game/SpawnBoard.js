7d9cd5ecf6a2f137ed5843c5ad36273ddcdd4117
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-03-25 16:57:46 +0545

    refactor Piece.js

--- game/SpawnBoard.js
+++ game/SpawnBoard.js
@@ -18,5 +18,16 @@ class SpawnBoard{
         this.ctx.fillRect(this.x,this.y,this.width,this.height);
         this.ctx.strokeStyle = "#222222";
         this.ctx.strokeRect(this.x,this.y,this.width,this.height);
+        this.drawBgBox();
+    }
+
+    drawBgBox(){
+        this.ctx.fillStyle = "#000000";
+        this.ctx.globalAlpha = 0.3;
+        let x = this.x + Math.floor((this.height*42)/120);
+        let y = this.y + Math.floor((this.height*20)/120);
+        let size = Math.floor((this.height*80)/120);
+        this.ctx.fillRect(x,y,size,size);
+        this.ctx.globalAlpha = 1;
     }
 }
